7113f50c1fec9b605518923aa7b2cdf77e85b245
Jacob-Gitt <jmcshrei@uci.edu>
2023-03-28 16:58:27 -0700

    implemented rock paper scissors

--- bot.py
+++ bot.py
@@ -1,22 +1,18 @@
 import discord
 import responses
 import os
+import random
 from dotenv import load_dotenv
-
+#load enviroment variables, these variables are used to store sensitive data
 load_dotenv()
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-
-    except Exception as e:
-        print(e)
-
+RPS_dict = dict()
+RPS_list = ["rock", "paper", "scissors"]
 
 def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
+    #sets client to my Client() object
     client = discord.Client(intents=intents)
 
     @client.event
@@ -25,19 +21,50 @@ def run_discord_bot():
 
     @client.event
     async def on_message(message):
+        #so bot does not respond to itself
         if message.author == client.user:
             return
 
+        #gather info about user
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
+        if username not in RPS_dict:
+            RPS_dict[username] = 0
+            RPS_dict[username + "bot"] = 0
+
         print(f'{username} said: "{user_message}" ({channel})')
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+        try:
+            response = responses.get_response(user_message)
+            #Play rock paper scissors
+            if response == "RPS":
+                #return rock, paper or scissors
+                computer_choice = RPS_list[random.randint(0,2)]
+
+                if user_message == computer_choice:
+                    await message.channel.send(f'Computer choice: {computer_choice}\nIt is a Tie!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "rock" and computer_choice == "scissors":
+                    RPS_dict[username] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\n{username} Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "rock" and computer_choice == "paper":
+                    RPS_dict[username + "bot"] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\nThe computer Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "paper" and computer_choice == "rock":
+                    RPS_dict[username] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\n{username} Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "paper" and computer_choice == "scissors":
+                    RPS_dict[username + "bot"] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\nThe computer Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "scissors" and computer_choice == "paper":
+                    RPS_dict[username] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\n{username} Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+                elif user_message == "scissors" and computer_choice == "rock":
+                    RPS_dict[username + "bot"] += 1
+                    await message.channel.send(f'Computer choice: {computer_choice}\nThe computer Wins!\nScore: {username} {RPS_dict[username]} | Computer {RPS_dict[username + "bot"]}')
+
+        except Exception as e:
+            print(e)
 
     client.run(os.getenv("token"))
--- responses.py
+++ responses.py
@@ -1,16 +1,13 @@
-import random
-
-
 def get_response(message: str) -> str:
-    p_message = message.lower()
+    lower_message = message.lower()
 
-    if p_message == 'hello':
-        return 'Hey there!'
+    if lower_message == "rock" or lower_message == "paper" or lower_message == "scissors":
+        return "RPS"
 
-    if message == 'roll':
-        return str(random.randint(1, 6))
+    if lower_message == "hangman":
+        return "HANGMAN"
 
-    if p_message == '!help':
+    if lower_message == '!help':
         return '`This is a help message that you can modify.`'
 
     return 'I didn\'t understand what you wrote. Try typing "!help".'
